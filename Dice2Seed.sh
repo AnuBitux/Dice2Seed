14dfb1e12a6eb522692241fbe67d27699e2563fe
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 22:34:41 +0100

    Update Dice2Seed.sh

--- Dice2Seed.sh
+++ Dice2Seed.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+echo ' '
+figlet Dice2Seed
+echo ' '
 echo 'Dice2Seed is a tool that helps users to generate a mnemonic seed from entropy obtained tracking dice rolls'
 echo 'It allows to insert the result of every roll or to paste the result of previously made rolls'
-echo 'To use it simply run Dice2Seed.py'
+echo 'To use it simply run dice2seed.py'
 echo 'CAUTION: preferably run it offline!'
 echo ' '
 $SHELL
